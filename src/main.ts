42e7a61b40acbe52fc14d70cbd8a984ce623e0b9
Bella Nur Hidayah <bellanurhidayah08@gmail.com>
2026-06-01 18:19:08 +0700

    ini yang rill

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
@@ -22,4 +24,4 @@ async function bootstrap() {
   await app.listen(port);
   console.log(`Aplikasi sukses berjalan di port: ${port}`);
 }
-bootstrap(); 
+bootstrap();
